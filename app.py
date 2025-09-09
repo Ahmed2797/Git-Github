a2a5d18aeb7e67abee39adc046a995e06c8cc02e
Ahmed2797 <tanvirahmed754575@gmail.com>
2025-09-09 17:52:22 +0600

    'notes

--- app.py
+++ app.py
@@ -3,4 +3,10 @@
 # git init
 # git add README.md(file_name)
 # git add .  # add all file
+# git commit -m 'message_sent_like_a_meanful
+# git push -u origin main
+# git branch
+# git branch second_branch  create_new_branch
+# git checkout second_branch  ----switch_branch
+# git branch -d second_branch
 # second_branch-----
